1d2441673a0b7bddd4a0d5b0a65fda6d2665d01a
Alexander Franz <alexfranz90@googlemail.com>
2022-11-16 09:04:49 +0100

    remove demo

--- demo/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Request
-from handler import router
-from fastapi.staticfiles import StaticFiles
-from fastapi.openapi.docs import get_swagger_ui_html
-
-app = FastAPI()
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
-
-app.include_router(router)
